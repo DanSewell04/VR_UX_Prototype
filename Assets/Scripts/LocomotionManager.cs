c2c5f187798a285d7ebfc66490f6e61c0e6425dc
Dan Sewell <dansewell036@gmail.com>
2025-04-16 13:17:58 +0100

    Added visual cue for active buttons

--- Assets/Scripts/LocomotionManager.cs
+++ Assets/Scripts/LocomotionManager.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit.Locomotion.Teleportation;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Locomotion.Movement;
 
 public class LocomotionManager : MonoBehaviour
 {
-    public GameObject teleportRay; 
+    public GameObject teleportRay;
     public ContinuousMoveProvider continuousMove;
     public TeleportationProvider teleportationProvider;
+
+    public Button freeMoveButton;
+    public Button teleportButton;
+
+    public Color activeColor = Color.green;
+    public Color inactiveColor = Color.white;
 
     public void SetLocomotionMode(bool useTeleport)
     {
@@ -16,12 +23,18 @@ public class LocomotionManager : MonoBehaviour
             teleportRay.SetActive(true);
             continuousMove.enabled = false;
             teleportationProvider.enabled = true;
+
+            teleportButton.image.color = activeColor;
+            freeMoveButton.image.color = inactiveColor;
         }
         else
         {
             teleportRay.SetActive(false);
             continuousMove.enabled = true;
             teleportationProvider.enabled = false;
+
+            freeMoveButton.image.color = activeColor;
+            teleportButton.image.color = inactiveColor;
         }
     }
 }
